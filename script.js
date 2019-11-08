228a535f87273eac18fcd405b55157c62483d14c
RodrosD <57403194+RodrosD@users.noreply.github.com>
2019-11-08 10:59:45 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var alumns = new Array ("alumn1", "alumn2", "alumn3", "alumn4", "alumn5", "alumn6", "alumn7", "alumn8", "alumn9", "alumn10", "alumn11", "alumn12", "alumn13", "alumn14", "alumn15", "alumn16", "alumn17", "alumn18", "alumn19", "alumn20", "alumn21", "alumn22", "alumn23", "alumn24")
+            
+           /* //para escribir el nombre random
+            var randomItem = nombres[Math.floor(Math.random()*nombres.length)];
+            document.body.innerHTML = randomItem + "<br>" + "<br>";
+
+            //para comprobar que carcel es igual a random y que añade carcel al final de la lista
+            var carcel = new Array(randomItem);  
+            nombres.push(randomItem)
+            document.write(nombres[24] + "<br>" + "<br>");
+            
+            //para escribir todos los nombres
+            for (i=0; i<nombres.length; i++)
+                document.write(nombres[i]+"<br>") */
